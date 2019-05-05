08335b92e4f635540706b975bb7b0ce7e05f09fe
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2019-05-05 15:44:57 -0500

    Removed/added cases to match requirements

--- run_sim.sh
+++ run_sim.sh
@@ -6,17 +6,5 @@ if [ "$#" -ne 1 ]; then
     echo "invalid number of argument"
 fi
 
-run.py -s 16KB -a 32 -f $1
-
-run.py -s 32KB -a 32 -f $1
-
-run.py -s 32KB -a 16 -f $1
-
-run.py -s 32KB -a 8 -f $1
-
-run.py -s 256KB -a 16 -f $1
-
-run.py -s 4MB -a 16 -f $1
-
-run.py -s 4MB -a 8 -f $1
+run.py -s 1MB -a 16 -f $1
 
